48df2069b995a8d0f39ce6a28dcc1015a0fb8de2
Alba De biase <debiasealba@gmail.com>
2020-12-02 16:01:49 +0100

    position absolute counter

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var app = new Vue ({
     el:'#container',
     data: {
+        header:["Courses","Courses Formats","Add Corses","Pages", "Demos"],
         menu:["about","contact","pages","blog"],
         pages:["Blog","Home","Shortcodes","Couses", "Membership","Tipografy"]
     }
