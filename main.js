69c59b0d8e67a9efdcfb0f9b0553a4e13fa08086
Alba De biase <debiasealba@gmail.com>
2020-12-02 15:24:15 +0100

    footer dinamico prove

---
+++ main.js
@@ -0,0 +1,7 @@
+var app = new Vue ({
+    el:'#container',
+    data: {
+        menu:["about","contact","pages","blog"],
+        pages:["Blog","Home","Shortcodes","Couses", "Membership","Tipografy"]
+    }
+})
