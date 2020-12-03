b780d7ced42d88359c7a50be08ef73184ff9ed2e
Alba De biase <debiasealba@gmail.com>
2020-12-03 12:06:20 +0100

    aggiunta right-fixed-bar

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var app = new Vue ({
     el:'#wrap',
     data: {
-        header:["Courses","Courses Formats","Add Corses","Pages", "Demos"],
+        header:["Courses","Course Formats","Add Corses","Pages", "Demos"],
         pages:["Blog","Home","Shortcodes","Couses", "Membership","Tipografy"]
     }
 })
