526d81fe2c3544cab2eda5f1fa943c173dc019a7
Alba De biase <debiasealba@gmail.com>
2020-12-03 02:00:00 +0100

    sistemato footer

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ var app = new Vue ({
     el:'#wrap',
     data: {
         header:["Courses","Courses Formats","Add Corses","Pages", "Demos"],
-        menu:["about","contact","pages","blog"],
         pages:["Blog","Home","Shortcodes","Couses", "Membership","Tipografy"]
     }
 })
