00611eca18267d62cf134f2f3120b49e72cc8ae0
Alba De biase <debiasealba@gmail.com>
2020-12-03 15:59:03 +0100

    effetti hover

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var app = new Vue ({
     el:'#wrap',
     data: {
         header:["Courses","Course Formats","Add Corses","Pages", "Demos"],
-        pages:["Blog","Home","Shortcodes","Couses", "Membership","Tipografy"]
+        pages:["Blog","Home","Shortcodes","Couses", "Membership","Tipografy"],
+    
     }
 })
